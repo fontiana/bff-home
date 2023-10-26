e8cb40eadda9d3de62cee9df68a0f4a765c93880
Victor Oliveira <victor.oliveira@avanade.com>
2023-10-26 09:59:26 -0300

    fix: melhorando cookie

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const port = 8080; // Escolha a porta que desejar
 
-console.log("iniciando aplicacao")
+app.use(cookieParser());
 
 // Middleware para lidar com JSON
 app.use(express.json());
@@ -17,7 +19,7 @@ app.get('/api/cookie', (req, res) => {
   const data = req.body;
   // Faça algo com os dados recebidos
   console.log(data);
-  res.cookie('cokkieName', 66, { maxAge: 900000, httpOnly: true, domain: "azurewebsites.net" })
+  res.cookie('TESTE', "Hello World!", { httpOnly: true, domain: "azurewebsites.net", sameSite: 'strict', secure: true })
   res.json({ message: 'Dados recebidos com sucesso!' });
 });
 
